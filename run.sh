68647c06cf0d49de7de7d2071643e2b155f559c8
atsushi totsuka <a.totsuka@makip.co.jp>
2018-12-22 05:12:07 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,18 +1,48 @@
-#!/bin/sh
+#!/bin/sh -eux
 
-rm -rf /etc/ssl/demoCA/*
-mkdir -p /etc/ssl/demoCA/private\
- && mkdir -p /etc/ssl/demoCA/newcerts\
- && mkdir -p /etc/ssl/demoCA/server
+function make_base(){
+	local CATOP=$1
+  rm -rf ${CATOP}
+  mkdir ${CATOP}
+  mkdir "${CATOP}/certs"
+  mkdir "${CATOP}/crl"
+  mkdir "${CATOP}/newcerts"
+  mkdir "${CATOP}/private"
+  touch "${CATOP}/index.txt"
+  echo 01 > "${CATOP}/crlnumber"
+  echo 01 > "${CATOP}/serial"
+}
+rm -rf ./dist
+rm -rf ./tmp
+mkdir -p ./dist/server
+mkdir -p ./dist/client
+mkdir -p ./tmp
 
-cd /etc/ssl/demoCA
-openssl genrsa -out ./private/cakey.pem 2048\
- && echo -e "\n\n\n\n\n*.local\n\nprivate\n\n" | openssl req -new -key ./private/cakey.pem -out cacert.csr\
- && openssl x509 -in cacert.csr -req -signkey ./private/cakey.pem -out cacert.pem\
- && touch index.txt\
- && echo 00 > serial
-cd /etc/ssl/
-openssl genrsa -out ./demoCA/server/serverkey.pem 2048\
- && echo -e "\n\n\n\n\n*.amazonaws.com\n\nprivate\n\n" | openssl req -new -key ./demoCA/server/serverkey.pem -out ./demoCA/server/server.csr\
- && yes | openssl ca -out ./demoCA/server/servercert.pem -infiles ./demoCA/server/server.csr\
- && openssl x509 -in ./demoCA/server/servercert.pem -text
+make_base ./CA
+make_base ./ICA
+
+openssl genrsa -out ./CA/private/cakey.pem 2048
+echo -e "\n\n\n\n\n\n\nprivate\n\ny\n" | openssl req -config ./openssl.cnf.ca -new -key ./CA/private/cakey.pem -out ./CA/careq.pem
+openssl ca -config ./openssl.cnf.ca -create_serial -out ./CA/cacert.pem -days 1095 -batch -keyfile ./CA/private/cakey.pem -selfsign -extensions v3_ca  -infiles ./CA/careq.pem
+openssl x509 -in CA/cacert.pem -out dist/client/ca.crt
+
+openssl genrsa -out tmp/newkey.pem 2048
+echo -e "\n\n\n\n\n\n\nprivate\n\n" | openssl req -config ./openssl.cnf.ica -new  -key tmp/newkey.pem -out tmp/newreq.pem -days 3650
+
+echo -e "y\ny\nprivate\nprivate\n" | openssl ca -config ./openssl.cnf.ca -policy policy_anything -out tmp/newcert.pem -extensions v3_ca  -infiles tmp/newreq.pem
+
+
+cp ./CA/cacert.pem ./ICA/cacert.pem
+cp ./CA/private/cakey.pem ./ICA/private/cakey.pem
+
+openssl x509 -in tmp/newcert.pem -out dist/server/ica.crt
+openssl x509 -in ./ICA/cacert.pem -out dist/client/ica.crt
+
+
+openssl genrsa 2048 > tmp/newkey.pem
+openssl rsa -in tmp/newkey.pem -out tmp/newkey.pem
+echo -e "\n\n\n\n\n*.amazonaws.com\n\nprivate\n\n" | openssl req -new -key tmp/newkey.pem -out tmp/newreq.pem
+echo -e "y\ny\nprivate\nprivate\n" | openssl ca -config ./openssl.cnf.ica -policy policy_anything -out tmp/newcert.pem  -infiles tmp/newreq.pem
+openssl x509 -in tmp/newcert.pem -out dist/server/server.crt
+mv tmp/newkey.pem dist/server/server.key
+cat dist/server/server.crt dist/server/ica.crt > dist/server/server.nginx.crt
